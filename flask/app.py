910e5f274d9aae85ecb0114e0de66e85d2c84824
Chen DingXiao <aaacdx@gmail.com>
2018-01-05 01:17:12 +0800

    add sendSMS route

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify
 from flask import request
+import random
+import SMSService
 
 app = Flask(__name__)
 
@@ -15,9 +17,11 @@ def index():
     return render_template('login.html')
 
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['POST','GET'])
 def login():
-    
+    if request.method == 'GET':
+        return render_template('login.html')
+
     username = request.form['username']
     password = request.form['password']
     
@@ -25,11 +29,32 @@ def login():
         return redirect(url_for('editor'))
     return redirect(url_for('index'))
 
-@app.route("/register", methods=['POST','GET'])
+@app.route("/regist", methods=['POST','GET'])
 def regist():
-    return render_template('register.html')
-    if request.methods == 'GET':
+    print(request.form)
+    print(request.method)
+    if request.method == 'GET':
         print(url_for('regist'))
+        return render_template('register.html')
+    if request.method == 'POST':
+        #TODO add db.
+        username = request.form['username']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+        telephone = request.form['phone']
+        print('username={} passowrd={}'.format(username,password1))
+        # return redirect(url_for('index'))
+    return u'error method'
+
+#TODO send SMS code
+@app.route("/sendSMS", methods=['POST'])
+def sendSMS():
+    print('-------sendSMS------')
+    username = request.form['username']
+    telephone = request.form['phone']
+    verifycode = random.randint(100000,999999)
+    SMSService.sendSms(str(telephone), str(verifycode))
+    return jsonify({'status':'OK', 'username':username, 'phone':telephone})
 
 @app.route("/agreement")
 def agreement():
